e994923f757f55ace8ff2221af48c62ed09bfe26
Henning Hasemann <henning@hasemail.de>
2015-06-02 19:57:56 +0200

    somewhat more useful check_compile.sh script

--- boards/control_module/software/system/README.txt
+++ boards/control_module/software/system/README.txt
@@ -4,15 +4,19 @@ release and run it:
 
 Put into /etc/crontab:
 
-*/1 * * * * pinball	/home/pinball/check_compile.sh >/home/pinball/log/cron.log 2>&1
+	*/1 * * * * pinball	/home/pinball/check_compile.sh >/home/pinball/log/cron.log 2>&1
 
 
 Allow the pinball user to run the pinball_controller software as root
 (necessary for some permission thingies):
 
+
 Put into /etc/sudoers:
 
-pinball ALL=(ALL) NOPASSWD: /home/pinball/pinball/boards/control_module/software/pinball_controller
+	pinball ALL=(ALL) NOPASSWD: /home/pinball/pinball/boards/control_module/software/pinball_controller
 
 
+Put into /etc/rc.local:
+
+	rm /home/pinball/check_compile.lock
 
--- boards/control_module/software/system/check_compile.sh
+++ boards/control_module/software/system/check_compile.sh
@@ -5,17 +5,7 @@ REPO_URL=https://github.com/Droggelbecher/pinball.git
 REPO_DIR=/home/pinball/pinball
 LOCKFILE=/home/pinball/check_compile.lock
 
-if [ -e $LOCKFILE ]; then
-	echo "$LOCKFILE exists, not running compile/run check."
-	exit 1;
-fi
-
-touch $LOCKFILE
-
-if [ ! -e $LOCKFILE ]; then
-	echo "$LOCKFILE does not exist right after touching it WTF?"
-	exit 1;
-fi
+LOGFILE=/home/pinball/log/check_compile.log
 
 function online() {
 	ping -c 4 $ONLINE_HOST
@@ -33,24 +23,61 @@ function not() {
 	fi
 }
 
+function log() {
+	echo $(date +'[%Y-%m-%d %H:%M]') $@ >> $LOGFILE
+}
+
+function loglines() {
+	while read n; do
+		log $n
+	done
+}
+
+log "$0 starting."
+
 if [ "$(hostname)" != "raspberrypi" ]; then
-	echo "Hostname is not 'pinball'. Sure you know what you are doing?"
+	log "Hostname is not 'pinball'. Sure you know what you are doing?"
 	exit 1;
 fi
 
-if not running; then
-	if [ -e $REPO_DIR ]; then
-		cd $REPO_DIR
-		git pull
+if [ -e $LOCKFILE ]; then
+	log "$LOCKFILE exists, not running compile/run check."
+	exit 1;
+fi
 
-	elif online; then
-		mkdir -p $REPO_DIR
-		git clone $REPO_URL $REPO_DIR
+
+touch $LOCKFILE
+
+if not running; then
+	log "Controller doesnt seem to be running."
+
+	if online; then
+		log "It seems we are online :-)"
+
+		if [ -e $REPO_DIR ]; then
+			log "Repo exists, pulling for news..."
+
+			cd $REPO_DIR
+			git pull
+
+		else
+			log "No repo found, cloning..."
+			mkdir -p $REPO_DIR
+			git clone $REPO_URL $REPO_DIR
+		fi
 	fi
 
 	cd $REPO_DIR/boards/control_module/software
-	make
-	sudo ./pinball_controller >/dev/null 2>&1
+
+	log "Trying to compile..."
+	make 2>&1 | loglines
+
+	if not running; then
+		log "Still not running, lets give it a go :-)"
+		sudo ./pinball_controller 2>&1 | loglines
+	fi
+else
+	log "Controller already running, not doing anything."
 fi
 
 rm $LOCKFILE
