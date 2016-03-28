fb3311788ffb4ff4d44af569a707f0749419f781
Henning Hasemann <henning@hasemail.de>
2016-03-28 20:44:12 +0200

    little fixes

--- boards/control_module/software/src/canvas/scrolling_canvas.cc
+++ boards/control_module/software/src/canvas/scrolling_canvas.cc
@@ -37,22 +37,9 @@ void ScrollingCanvas::set(Coordinate<> c, uint8_t color) {
 
 	else if(offset_.column() + decorated_.size().column() > size().column()
 			&& c.column() < offset_.column() + decorated_.size().column() - size().column()) {
-		//std::cout
-			//<< decorated_.size().row() << " " << decorated_.size().column() << " "
-			//<< size().row() << " " << size().column() << " "
-			//<< c.row() << " " << c.column() << " "
-			//<< offset_.row() << " " << offset_.column() << std::endl;
-
-		//assert( decorated_.size().contains(c + size() - offset_) );
 		Coordinate<> cc = (c + size() - offset_) % decorated_.size();
 
-		if(decorated_.size().contains(cc)) {
 		decorated_.set(cc, color);
-		}
-				//c + size() - offset_ - decorated_.size(), color);
-		//decorated_.set(
-				//Coordinate<>(c.row(), c.column() + size().column() - offset_.column()), color);
-		//decorated_.set(c, color);
 	}
 }
 
