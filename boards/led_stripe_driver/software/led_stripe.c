d01dac174d230e0e2160b560231b12571a6aa2c2
Henning Hasemann <henning@automotive-ai.com>
2019-03-24 16:13:54 +0100

    implemented some led strip coloring commands

--- boards/led_stripe_driver/software/led_stripe.c
+++ boards/led_stripe_driver/software/led_stripe.c
@@ -4,41 +4,143 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
+#include <string.h>
 
 /*#include <checksum.h>*/
 
 #include "led_stripe.h"
 
-#define LEDS 6
+#define LEDS 10
 struct cRGB led[LEDS];
 
+volatile unsigned long milliseconds;
+
+// Command execution state
+int current_led = 0;
+unsigned long next_action_ms = 0;
 
 int main(void) {
-	setup_pins();
+	setup();
 
 	for(int i = 0; i < LEDS; i++) {
 		led[i].r = led[i].g = led[i].b = 0;
 	}
+	ws2812_setleds(led, LEDS);
+
+	Command yellow = {
+		.mode = FULL,
+		.arg_R0 = 0xff,
+		.arg_G0 = 0xa0,
+		.arg_B0 = 0x00
+	};
+
+	Command chaser = {
+		.mode = CHASER,
+		.arg_R0 = 0xff,
+		.arg_G0 = 0xa0,
+		.arg_B0 = 0x00,
+		.arg_DT = 60, // *10ms
+		.arg_MOD = 3 //LEDS / 2
+	};
+
+	Command fade = {
+		.mode = FADE,
+		.arg_R0 = 0xf0,
+		.arg_G0 = 0xa0,
+		.arg_B0 = 0x00,
+
+		.arg_R1 = 0x10,
+		.arg_G1 = 0x20,
+		.arg_B1 = 0xa0
+	};
+
 
 	while(1) {
-		led[0].r++;
-		led[1].g++;
-		led[2].b++;
-
-		led[3].r++; led[3].g++;
-		led[4].g++; led[4].b++;
-		led[5].b++; led[5].r++;
-
-		ws2812_setleds(led, LEDS);
-		_delay_ms(100);
+		execute(&fade);
+		_delay_ms(1);
 
 	}
 
 	return 0;
 }
 
-void setup_pins(void) {
-	/*DDRF |= (1 << PF0);*/
+void clear_leds() {
+	/*memset(&led, 0, LEDS * sizeof(struct cRGB));*/
+	for(int i = 0; i < LEDS; i++) {
+		led[i].r = led[i].g = led[i].b = 0;
+	}
+}
+
+void execute(Command* c) {
+	switch(c->mode) {
+		case FULL:
+			for(int i = 0; i < LEDS; i++) {
+				led[i].r = c->arg_R0;
+				led[i].g = c->arg_G0;
+				led[i].b = c->arg_B0;
+			}
+			break;
+
+		case CHASER:
+			if(milliseconds >= next_action_ms) {
+				next_action_ms = milliseconds + c->arg_DT * 10;
+				current_led++;
+				if(current_led >= LEDS / 2) {
+					current_led = 0;
+				}
+			}
+
+			clear_leds();
+
+			for(int i = 0; i < LEDS / 2; i++) {
+				if((i + current_led) % c->arg_MOD == 0) {
+				led[i].r = c->arg_R0;
+				led[i].g = c->arg_G0;
+				led[i].b = c->arg_B0;
+
+				led[LEDS - i - 1].r = c->arg_R0;
+				led[LEDS - i - 1].g = c->arg_G0;
+				led[LEDS - i - 1].b = c->arg_B0;
+				}
+			}
+
+			break;
+
+		case FADE:
+			for(int i = 0; i < LEDS/2; i++) {
+				led[i].r = c->arg_R0 + i * ((int)c->arg_R1 - (int)c->arg_R0) / (LEDS/2 - 1);
+				led[i].g = c->arg_G0 + i * ((int)c->arg_G1 - (int)c->arg_G0) / (LEDS/2 - 1);
+				led[i].b = c->arg_B0 + i * ((int)c->arg_B1 - (int)c->arg_B0) / (LEDS/2 - 1);
+				led[LEDS - i - 1] = led[i];
+			}
+			break;
+
+
+	} // switch
+	ws2812_setleds(led, LEDS);
+} // execute()
+
+ISR(TIMER1_COMPA_vect)
+{
+	++milliseconds;
+}
+
+void setup(void) {
+	// LED data out pin
 	DDRC |= (1 << PC0);
+
+
+	cli();
+	// CTC mode, Clock/8
+	TCCR1B |= (1 << WGM12) | (1 << CS11);
+ 
+	// Load the high byte, then the low byte
+	// into the output compare
+	OCR1AH = (CTC_MATCH_OVERFLOW >> 8);
+	OCR1AL = CTC_MATCH_OVERFLOW;
+ 
+	// Enable the compare match interrupt
+	TIMSK1 |= (1 << OCIE1A);
+	sei();
 }
 
--- boards/led_stripe_driver/software/led_stripe.h
+++ boards/led_stripe_driver/software/led_stripe.h
@@ -5,17 +5,63 @@
 //#include <unistd.h> // usleep
 //#include <util/delay.h>
 
-#include "light_ws2812_config.h"
+#include <stdint.h>
 
+#include "light_ws2812_config.h"
 #include "light_ws2812.h"
+
+
+// 16MHz Clock
+#define F_CPU 16000000UL
+ 
+// Calculate the value needed for 
+// the CTC match value in OCR1A.
+#define CTC_MATCH_OVERFLOW ((F_CPU / 1000) / 8) 
 
 // PF0 = Analog 0
 // PC0 = Diginal 37
 // src: https://www.arduino.cc/en/uploads/Hacking/PinMap2560big.png
 
+#define arg_R0 arg0
+#define arg_G0 arg1
+#define arg_B0 arg2
+#define arg_DT arg3
+#define arg_MOD arg4
+
+#define arg_R1 arg4
+#define arg_G1 arg5
+#define arg_B1 arg6
+
+/**
+ * MODE       ARG0  ARG1  ARG2  ARG3  ARG4  ARG5  ARG6
+ *
+ * FULL       Color
+ * CHASER
+ */
+
+typedef enum {
+	FULL = 0,
+	CHASER,
+	FADE
+} Mode;
+
+typedef struct {
+	uint8_t mode;
+
+	// Note: turning these into an array might cause issues with alignment
+	uint8_t arg0;
+	uint8_t arg1;
+	uint8_t arg2;
+	uint8_t arg3;
+	uint8_t arg4;
+	uint8_t arg5;
+	uint8_t arg6;
+} Command;
 
 int main(void);
-void setup_pins(void);
+void setup(void);
+void execute(Command*);
+void clear_leds(void);
 
 #endif
 
